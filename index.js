3bfa994d3084cd85d8693fd0f18d1474d7464f43
Adib Saad <adib@airpr.com>
2019-03-08 20:39:13 -0500

    Update addon - General ember updates - Move main logic to prepare/didPrepare - Register a honeybadger deployment in didDeploy - Use request-promise instead of raw curl commands

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 /* jshint node: true */
 'use strict';
 
+var BasePlugin = require('ember-cli-deploy-plugin');
 var RSVP = require('rsvp');
 var fs = require('fs');
 var path = require('path');
-var merge = require('lodash/object/merge');
 var template = require('lodash/string/template');
 var minimatch = require('minimatch');
-var FormData = require('form-data');
-var exec = require('child_process').exec;
-var zlib = require('zlib');
+var request = require('request-promise');
 
-var BasePlugin = require('ember-cli-deploy-plugin');
+const metaContent = '<meta name="honeybadger"/>';
+const sourceMapApiURL = 'https://api.honeybadger.io/v1/source_maps';
+const deploymentApiURL = 'https://api.honeybadger.io/v1/deploys';
 
 module.exports = {
-  name: 'airpr-honeybadger',
+  name: 'ember-cli-deploy-honeybadger',
 
   createDeployPlugin: function(options) {
     var DeployPlugin = BasePlugin.extend({
-      name: "honeybadger",
+      name: options.name,
+      runAfter:  ['revision-data'],
+      requiredConfig: ['apiKey', 'minifiedPrependUrl'],
 
       defaultConfig: {
         projectName: function(context) {
@@ -30,9 +32,6 @@ module.exports = {
         distFiles: function(context) {
           return context.distFiles;
         },
-        gzippedFiles: function(context) {
-          return context.gzippedFiles || []; // e.g. from ember-cli-deploy-gzip
-        },
         distDir: function(context) {
           return context.distDir;
         },
@@ -42,80 +41,32 @@ module.exports = {
           var environment = honeybadgerConfig ? honeybadgerConfig.environment : false;
           return environment || buildConfig.environment || 'production';
         },
-        enabled: function(context) {
-          var honeybadgerConfig = context.config.honeybadger.honeybadgerConfig;
-          var enabled = honeybadgerConfig ? honeybadgerConfig.enabled : true;
-          return !(enabled === false);
-        },
-        captureUncaught: function(context) {
-          var honeybadgerConfig = context.config.honeybadger.honeybadgerConfig;
-          var captureUncaught = honeybadgerConfig ? honeybadgerConfig.captureUncaught : true;
-          return !(captureUncaught === false);
-        },
-        integrateHoneybadger: true,
         additionalFiles: [],
         honeybadgerFileURI: '//js.honeybadger.io/v0.5/honeybadger.min.js'
       },
-      requiredConfig: ['accessToken', 'accessServerToken', 'minifiedPrependUrl'],
 
-      willUpload: function(context) {
-        if(this.readConfig('integrateHoneybadger')) {
-          // setup honeybadgerConfig
-          var honeybadgerConfig = {
-            accessToken: this.readConfig('accessToken'),
-            enabled: this.readConfig('enabled'),
-            captureUncaught: this.readConfig('captureUncaught'),
-            environment: this.readConfig('environment'),
-            codeVersion: this.readConfig('revisionKey'),
-            payload: {
-              client: {
-                javascript: {
-                  source_map_enabled: true,
-                  code_version: this.readConfig('revisionKey'),
-                  guess_uncaught_frames: true
-                }
-              }
-            }
-          };
+      prepare: function(context) {
+        // render honeybadger snippet with fulfilled config
+        var htmlSnippetPath = path.join(__dirname, 'addon', 'honeybadger.html');
+        var htmlContent = fs.readFileSync(htmlSnippetPath, 'utf-8');
 
-          var honeybadgerFileURI = this.readConfig('honeybadgerFileURI');
+        var honeybadgerSnippet = template(htmlContent)({
+          apiKey: this.readConfig('apiKey'),
+          environment: this.readConfig('environment'),
+          revision: this.readConfig('revisionKey'),
+          jsFile: this.readConfig('honeybadgerFileURI')
+        });
 
-          // render honeybadger snippet with fulfilled config
-          var htmlSnippetPath = path.join(__dirname, 'addon', 'honeybadger.html');
-          var htmlContent = fs.readFileSync(htmlSnippetPath, 'utf-8');
-
-          var honeybadgerSnippet = template(htmlContent)({
-            apiKey: honeybadgerConfig.accessToken,
-            environment: honeybadgerConfig.environment,
-            revision: honeybadgerConfig.codeVersion,
-            jsFile: honeybadgerFileURI
-          });
-
-          // replace honeybadger metatag with honeybadger snippet in index.html
-          var indexPath = path.join(context.distDir, "index.html");
-          var index = fs.readFileSync(indexPath, 'utf8');
-          index = index.replace('<meta name="honeybadger"/>', honeybadgerSnippet);
-          fs.writeFileSync(indexPath, index);
-        }
+        // replace honeybadger metatag with honeybadger snippet in index.html
+        var indexPath = path.join(context.distDir, "index.html");
+        var index = fs.readFileSync(indexPath, 'utf8');
+        index = index.replace(metaContent, honeybadgerSnippet);
+        fs.writeFileSync(indexPath, index);
       },
 
-      _isFileGzipped(filePath) {
-        var gzippedFiles = this.readConfig('gzippedFiles');
-        return gzippedFiles.indexOf(filePath.replace("tmp/deploy-dist/", "")) >= 0;
-      },
-
-      _unzipPath(filePath){
-        var newPath = filePath + ".unzipped";
-        var readStream = fs.createReadStream(filePath);
-        var writeStream = fs.createWriteStream(newPath);
-
-        readStream
-          .pipe(zlib.createGunzip())
-          .pipe(writeStream);
-        return newPath;
-      },
-
-      upload: function(context) {
+      // upload files in didPrepare before ember-cli-deploy-gzip and related
+      // plugins gzip our files
+      didPrepare: function(context) {
         var distFiles = this.readConfig('distFiles');
         var projectName = this.readConfig('projectName');
         var additionalFiles = this.readConfig('additionalFiles');
@@ -135,12 +86,8 @@ module.exports = {
         }));
 
         var promiseArray = [];
-        var accessToken = this.readConfig('accessToken');
-        var revisionKey = this.readConfig('revisionKey');
 
         for(var i = 0; i < projectFileJs.length; i++) {
-          // upload map to honeybadger using form-data
-
           var mapFilePath = path.join(this.readConfig('distDir'), projectFileMap[i]);
           var jsFilePath = path.join(this.readConfig('distDir'), projectFileJs[i]);
           var minifiedPrependUrl = this.readConfig('minifiedPrependUrl');
@@ -148,34 +95,47 @@ module.exports = {
             minifiedPrependUrl = minifiedPrependUrl(context);
           }
           [].concat(minifiedPrependUrl).forEach((url)=> {
+            var formData = {
+              name: url + projectFileJs[i],
+              api_key: this.readConfig('apiKey'),
+              revision: this.readConfig('revisionKey'),
+              minified_url: url + projectFileJs[i],
+              source_map: fs.createReadStream(mapFilePath),
+              minified_file: fs.createReadStream(jsFilePath)
+            };
 
-            if(this._isFileGzipped(mapFilePath)){
-              mapFilePath = this._unzipPath(mapFilePath);
-            }
-
-            if(this._isFileGzipped(jsFilePath)){
-              jsFilePath = this._unzipPath(jsFilePath);
-            }
-
-            var curlArgs = ['https://api.honeybadger.io/v1/source_maps',
-                            '-F api_key=' + accessToken,
-                            '-F revision=' + revisionKey,
-                            '-F minified_url=' + url + projectFileJs[i],
-                            '-F source_map=@' + mapFilePath,
-                            '-F minified_file=@' + jsFilePath].join(" ");
-            var promise = new RSVP.Promise(function(resolve, reject) {
-              exec('curl ' + curlArgs, function (error, stdout, stderr) {
-                if (error !== null) {
-                  reject(error);
-                }else{
-                  resolve();
-                }
-              });
-            });
-            promiseArray.push(promise);
+            promiseArray.push(request({
+              uri: sourceMapApiURL,
+              method: 'POST',
+              formData: formData
+            }));
           });
         };
+
+        this.log('Uploading files...', {verbose: true});
         return RSVP.all(promiseArray);
+      },
+
+      didDeploy: function() {
+        var deploy = {
+          environment: this.readConfig('environment'),
+          revision: this.readConfig('revisionKey')
+        };
+
+        var username = this.readConfig('username');
+        if(username){
+          deploy.username = username;
+        }
+
+        this.log('Registering deployment...', {verbose: true});
+        return request({
+          method: 'POST',
+          uri: deploymentApiURL,
+          form: {
+            api_key: this.readConfig('apiKey'),
+            deploy: deploy
+          }
+        });
       }
     });
 
@@ -184,7 +144,7 @@ module.exports = {
 
   contentFor: function(type) {
     if (type === 'head') {
-      return '<meta name="honeybadger"/>';
+      return metaContent;
     }
   }
 };
